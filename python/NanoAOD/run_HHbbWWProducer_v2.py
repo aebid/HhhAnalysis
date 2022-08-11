642ee13717ee3a0b57894cd33d9ac5304f35cdb2
Devin <aebid@tamu.edu>
2022-08-11 21:43:17 +0200

    Updated BTagging

--- python/NanoAOD/run_HHbbWWProducer_v2.py
+++ python/NanoAOD/run_HHbbWWProducer_v2.py
@@ -9,15 +9,15 @@ import warnings
 import __builtin__
 #__builtin__.Runyear = 2016
 #Runyear = 2016
-#__builtin__.Runyear = 2017
+__builtin__.Runyear = 2017
 #Runyear = 2017
-__builtin__.Runyear = 2018
+#__builtin__.Runyear = 2018
 #Runyear = 2018
 
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
 
-sys.path.append('/afs/cern.ch/work/t/tahuang/HHAnalysis/CMSSW_10_2_0/src/HhhAnalysis/python/NanoAOD')
-#sys.path.append('/afs/cern.ch/work/d/daebi/diHiggs/CMSSW_10_2_0/src/HhhAnalysis/python/NanoAOD')
+#sys.path.append('/afs/cern.ch/work/t/tahuang/HHAnalysis/CMSSW_10_2_0/src/HhhAnalysis/python/NanoAOD')
+sys.path.append('/afs/cern.ch/work/d/daebi/diHiggs/CMSSW_10_2_26/src/HhhAnalysis/python/NanoAOD')
 #from countHistogramProducer import *
 from genParticleProducer import *
 #from HHbbWWProducer_sync import *
@@ -28,11 +28,15 @@ from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import 
 btagSF2016_cMVA = lambda : btagSFProducer("2016",  algo = 'cmva', sfFileName='cMVAv2_Moriond17_B_H.csv')## file is under NanoAODTools
 btagSF2017_deepCSV = lambda : btagSFProducer("2017", algo = 'deepcsv', sfFileName='DeepCSV_94XSF_V3_B_F.csv')
 btagSF2018_deepCSV = lambda : btagSFProducer("2018", algo = 'deepcsv', sfFileName='DeepCSV_102XSF_V1.csv')
+
+btagSF2016_deepjet = lambda : btagSFProducer("Legacy2016", algo = 'deepjet', sfFileName='DeepJet_2016LegacySF_V1.csv')
+btagSF2017_deepjet = lambda : btagSFProducer("2017", algo = 'deepjet', sfFileName='DeepFlavour_94XSF_V3_B_F.csv')
+btagSF2018_deepjet = lambda : btagSFProducer("2018", algo = 'deepjet', sfFileName='DeepJet_102XSF_V1.csv')
 def btagSFyear(year):
     return{
-	2016: btagSF2016_cMVA(),
-	2017: btagSF2017_deepCSV(),
-	2018: btagSF2018_deepCSV()
+	2016: btagSF2016_deepjet(),
+	2017: btagSF2017_deepjet(),
+	2018: btagSF2018_deepjet()
     }[year]
 
 from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jecUncertainties import *
@@ -68,18 +72,18 @@ mht_hh = lambda : mhtProducer( lambda j : j.pt > 20 and abs(j.eta) < 2.4,
 #outputdir   = "tao_oldsample_mytest/"
 #outputdir   = "add_cov_test/"
 #outputdir   = "mysample_test_m{mass}/".format(mass = mass)
-#outputdir = "sync/"
+outputdir = "sync/"
 #file2016Test = ["add_cov/NANOAODSIM_M-260_1.root"]
 #file2016Test = ["tao_oldsample/myNanoProdMc2016_NANO_brazos_20190807.root"]
-outputdir   = "/eos/user/t/tahuang/2021NtupleProduceTest/"
+#outputdir   = "/eos/user/t/tahuang/2021NtupleProduceTest/"
 #file2016Test = ["/eos/user/t/tahuang/NanoAODSamples_2019/myNanoProdMc2016_NANO_brazos_20190807.root"]
 #file2017Test = ["/eos/user/t/tahuang/NanoAODSamples_2019/myNanoProdMc2017_NANO_brazos.root"]
 #file2018Test = ["/eos/user/t/tahuang/NanoAODSamples_2019/myNanoProdMc2018_NANO_10_20190805Test.root"]
-run2syncfile = {
-2016: ["/eos/user/t/tahuang/Sync_2021/sync_2016_m750.root"],
-2017: ["/eos/user/t/tahuang/Sync_2021/sync_2017_m750.root"],
-2018: ["/eos/user/t/tahuang/Sync_2021/sync_2018_m750.root"]
-}
+
+file2016Test = ["sync/sync_2016_m750.root"]
+file2017Test = ["sync/sync_2017_m750.root"]
+file2018Test = ["sync/sync_2018_m750.root"]
+tt_test = ["/eos/user/t/tahuang/NanoAODSampleTest/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_RunIIFall17_NanoAOD.root"]
 
 #file2016Test = ["test/test_m300_nanohadd.root"]
 #file2016Test = ["sync/sync_2016_m750.root"]
@@ -87,12 +91,14 @@ run2syncfile = {
 #file2018Test = ["tao_oldsample/myNanoProdMc2018_NANO_10_20190805Test.root"]
 
 #modules = [ puWeightyear(Runyear), countHistogramAll_2016(), jetmetUncertaintiesyear(Runyear), btagSFyear(Runyear),  mht_hh(), HHbbWWProducer(True, verbose = 1) ]
-modules = [ puWeightyear(Runyear),   HHbbWWProducer(True, verbose = 1) ]
+modules = [ puWeightyear(Runyear), btagSFyear(Runyear), jetmetUncertaintiesyear(Runyear), HHbbWWProducer(True, verbose = 1) ]
+#modules = [puWeightyear(Runyear), HHbbWWProducer(True, verbose = 1) ]
 #p=PostProcessor(outputdir, filesTTbar2017,"1","keep_and_drop.txt", modules, friend = True, jsonInput = None, provenance=True)
 #p=PostProcessor(outputdir, filesDY1J2017,"1","keep_and_drop.txt", modules, friend = True, jsonInput = None, provenance=True)
 ##note: make minor change to postprocessor.py to change the default output TTree name:
 ## https://github.com/tahuang1991/nanoAOD-tools/blob/master/python/postprocessing/framework/postprocessor.py
-p=PostProcessor(outputdir, run2syncfile[Runyear],"1","keep_and_drop.txt", modules, friend = True, jsonInput = None, provenance=True, outtreeName="syncTree")
-print "run Postprocessor here "
+#p=PostProcessor(outputdir, tt_test,"1","keep_and_drop.txt", modules, friend = True, jsonInput = None, provenance=True, outtreeName="syncTree")
+p=PostProcessor(outputdir, file2017Test,"1","keep_and_drop.txt", modules, friend = True, jsonInput = None, provenance=True, outtreeName="syncTree")
+print("run Postprocessor here ")
 p.run()
 
